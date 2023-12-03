e0477f347647bd4818196739e7c12056c334094f
alexis-moins <sinus.nacre0v@icloud.com>
2023-12-03 20:44:34 +0100

    feat: finished day one

--- cmd/day_one/day_one.go
+++ cmd/day_one/day_one.go
@@ -1,12 +1,39 @@
 package main
 
-import "github.com/alexis-moins/advent-of-code-2023/internal/utils"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"unicode"
+
+	"github.com/alexis-moins/advent-of-code-2023/internal/utils"
+)
 
 func main() {
 	file, scanner := utils.GetInput("day-one")
 	defer file.Close()
 
+	sum := 0
+
 	for scanner.Scan() {
-		println(scanner.Text())
+		digits := []rune{}
+
+		for _, char := range scanner.Text() {
+			if unicode.IsDigit(char) {
+				digits = append(digits, char)
+			}
+		}
+
+		format := fmt.Sprintf("%c%c", digits[0], digits[len(digits)-1])
+
+		number, err := strconv.Atoi(format)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += number
 	}
+
+	println(sum)
 }
---
+++ justfile
@@ -0,0 +1,2 @@
+day-one:
+    @go run cmd/day_one/day_one.go
