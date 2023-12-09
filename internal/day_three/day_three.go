1bc9852d2de54e90dff3441c26c3f8e2d80e74b0
alexis-moins <sinus.nacre0v@icloud.com>
2023-12-09 20:41:58 +0100

    wip

--- internal/day_three/day_three.go
+++ internal/day_three/day_three.go
@@ -68,6 +68,7 @@ func (self *EngineSchematic) parseGrid() {
 				}
 			} else if char == GEAR_CHAR {
 				self.PotentialGears = append(self.PotentialGears, Position{x, y})
+                index = -1
 			} else if index != -1 {
 				value := line[index:x]
 				self.parts[Range{start: index, end: x - 1, y: y}] = value
@@ -82,6 +83,7 @@ func (self EngineSchematic) FindAdjacentEngineParts(position Position) []int {
 	parts := []int{}
 
 	for _range, value := range self.parts {
+        fmt.Printf("value: %v\n", value)
 		if _range.IsAdjacentTo(position) {
 			part, err := strconv.Atoi(value)
 
@@ -95,96 +97,3 @@ func (self EngineSchematic) FindAdjacentEngineParts(position Position) []int {
 
 	return parts
 }
-
-// func (self EngineSchematic) getAdjacentSlices(x, y int) []string {
-// 	slices := []string{}
-
-// 	startIndex := max(x-1, 0)
-// 	endIndex := min(xEnd+1, len(self.grid[y])-1)
-
-// 	if xStart != startIndex {
-// 		leftChar := self.grid[y][startIndex]
-// 		slices = append(slices, string(leftChar))
-// 	}
-
-// 	if y > 0 {
-// 		// Not on the top row
-// 		slices = append(slices, self.grid[y-1][startIndex:endIndex])
-// 	}
-
-// 	rightChar := self.grid[y][xEnd]
-// 	slices = append(slices, string(rightChar))
-
-// 	if y < len(self.grid)-1 {
-// 		// Not on bottom row
-// 		slices = append(slices, self.grid[y+1][startIndex:endIndex])
-// 	}
-
-// 	return slices
-// }
-
-// var re = regexp.MustCompile(`[\.\d]`)
-
-// func (self EngineSchematic) IsEnginePart(x, y int) bool {
-// 	slices := self.getAdjacentSlices(self.index, x, y)
-// 	fmt.Printf("slices: %v\n", slices)
-
-// 	for _, slice := range slices {
-// 		if specialChars := re.ReplaceAll([]byte(slice), []byte("")); len(specialChars) > 0 {
-// 			fmt.Printf("specialCharacters: %s\n", string(specialChars))
-// 			return true
-// 		}
-// 	}
-
-// 	return false
-// }
-
-// // mustParseDigit returns true if the engine should parse the digits that were found.
-// func (self EngineSchematic) mustParseDigit(char rune, x int, line string) bool {
-// 	return self.isParsingNumber() && (!unicode.IsDigit(char) || self.isLastCharacterOfLine(x, line))
-// }
-
-// func (self EngineSchematic) isLastCharacterOfLine(x int, line string) bool {
-// 	return x == len(line)-1
-// }
-
-// func (self EngineSchematic) isParsingNumber() bool {
-// 	return self.index != -1
-// }
-
-// func (self EngineSchematic) GetEngineParts() []int {
-// 	partNumbers := []int{}
-
-// 	for y, line := range self.grid {
-
-// 		for x, char := range line {
-// 			fmt.Printf("char: %s\n", string(char))
-// 			if unicode.IsDigit(char) && !self.isParsingNumber() {
-// 				self.index = x
-// 			} else if self.mustParseDigit(char, x, line) {
-// 				if self.IsEnginePart(x, y) {
-// 					var toConvert string
-
-// 					if unicode.IsDigit(char) && self.isLastCharacterOfLine(x, line) {
-// 						toConvert = self.grid[y][self.index:]
-// 					} else {
-// 						toConvert = self.grid[y][self.index:x]
-// 					}
-// 					fmt.Printf("number: %v\n", toConvert)
-
-// 					number, err := strconv.Atoi(toConvert)
-
-// 					if err != nil {
-// 						log.Fatal(err)
-// 					}
-
-// 					partNumbers = append(partNumbers, number)
-// 				}
-
-// 				self.index = -1
-// 			}
-// 		}
-// 	}
-
-// 	return partNumbers
-// }
--- justfile
+++ justfile
@@ -1,17 +1,17 @@
 day-one:
-    @go run cmd/day_one/day_one.go
+    @-go run cmd/day_one/day_one.go
 
 test-day-one:
-    @go test ./test/day_one -v
+    @-go test ./test/day_one -v
 
 day-two:
-    @go run cmd/day_two/day_two.go
+    @-go run cmd/day_two/day_two.go
 
 test-day-two:
-    @go test ./test/day_two -v
+    @-go test ./test/day_two -v
 
 day-three:
-    @go run cmd/day_three/day_three.go
+    @-go run cmd/day_three/day_three.go
 
 test-day-three:
     @-go test ./test/day_three -v
